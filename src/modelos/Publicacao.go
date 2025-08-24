2001ca2b0bc7848ebf7752ad7f7ca094f554d27c
Isaque Lopes <isaque.lopes047@gmail.com>
2025-08-24 18:59:45 -0300

    new

--- src/modelos/Publicacao.go
+++ src/modelos/Publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicacao representa uma publicacao feita por um usuario
 type Publicacao struct {
@@ -11,4 +15,31 @@ type Publicacao struct {
 	AutorNick string    `json:"autorNick,omitempty"`
 	Curtidas  string    `json:"curtidas"`
 	CriadoEm  time.Time `json:"criadoEm,omitempty"`
+}
+
+// Vai validar e formatar os campos
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+}
+
+func (publicacao Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O titulo não pode esta em branco")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O conteudo é obrigatorio e não pode esta em branco")
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
